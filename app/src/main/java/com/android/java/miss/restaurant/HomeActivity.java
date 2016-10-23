d80ae35e3c3ec5151005a8a758104cac996753f7
“Suada <suada.haji@andela.com>
2016-10-23 16:37:17 +0300

    Modify the Home Activity

--- app/src/main/java/com/android/java/miss/restaurant/HomeActivity.java
+++ app/src/main/java/com/android/java/miss/restaurant/HomeActivity.java
@@ -1,13 +1,29 @@
 package com.android.java.miss.restaurant;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class HomeActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
     setContentView(R.layout.activity_home);
+    setStatusBackgroundColor();
+  }
+
+
+  private void setStatusBackgroundColor() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().getDecorView().setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+      getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black_transparent));
+    }
   }
 }
